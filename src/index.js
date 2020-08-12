d9f9db2382d9e069564d3de23bfea7de679433e7
Alen Vlahovljak <alen_vlahovljak@hotmail.com>
2020-08-12 20:27:45 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server has started on Port ${PORT}`));
